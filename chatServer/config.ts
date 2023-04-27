b46893d674e3e21e2593c6a9f322ab3fdbbe2513
Grzegorz <osowskigrzegorz95@gmail.com>
2023-04-27 13:38:52 +0200

    Starting app

--- chatServer/config.ts
+++ chatServer/config.ts
@@ -1,2 +1,2 @@
 export const port: any = process.env.PORT || 3001;
-export const database: string = process.env.DATABASE || 'mongodb://localhost:27017/chat';
+export const database: string = process.env.DATABASE || 'mongodb://root:root_password@localhost:27017/';
