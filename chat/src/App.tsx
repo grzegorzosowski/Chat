abd794e9abd4368f41d14c861af37df2edff2067
Grzegorz <osowskigrzegorz95@gmail.com>
2023-05-29 13:46:06 +0200

    UserProvider move down in tree

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -3,17 +3,20 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { apiSlice } from './features/api/apiSlice';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
-import { CurrentThemeProvider } from './features/currentThemeProvider';
 import Layout from './components/Layout';
+import { CurrentThemeProvider } from './features/currentThemeProvider';
+import { UserProvider } from './UserProvider';
 
 export default function App() {
   return (
     <ApiProvider api={apiSlice}>
       <Provider store={store}>
         <CurrentThemeProvider>
-          <SnackbarProvider maxSnack={3} autoHideDuration={3000} >
-            <Layout />
-          </SnackbarProvider>
+          <UserProvider>
+            <SnackbarProvider maxSnack={3} autoHideDuration={3000} style={{ fontFamily: 'Roboto, sans-serif' }} >
+              <Layout />
+            </SnackbarProvider>
+          </UserProvider>
         </CurrentThemeProvider>
       </Provider>
     </ApiProvider>
