10401fc49a40a570b442cd8c9d94aa9c2548d6d1
Grzegorz <osowskigrzegorz95@gmail.com>
2023-05-29 13:51:40 +0200

    Visual improvement

--- chat/src/components/validators/NickValidator.tsx
+++ chat/src/components/validators/NickValidator.tsx
@@ -9,7 +9,7 @@ type NickValidatorProps = {
 export default function NickValidator({ nick }: NickValidatorProps) {
   const { hasLengthCorrect, hasWhiteListChars } = nickValidation(nick)
   return (
-    <Box sx={{ border: '1px solid', borderColor: 'gray', borderRadius: '5px', padding: '10px', width: '100%', boxSizing: 'border-box' }}>
+    <Box sx={{ mb: '10px', border: '1px solid', borderColor: 'gray', borderRadius: '5px', padding: '10px', width: '100%', boxSizing: 'border-box' }}>
       <Typography variant='subtitle1' sx={{ color: 'gray', mb: '10px' }}>Nick requirements:</Typography>
       <Condition value={hasLengthCorrect}>Length 3-20 characters</Condition>
       <Condition value={hasWhiteListChars}>Allowed characters: [ a-zA-Z0-9_ ]</Condition>
