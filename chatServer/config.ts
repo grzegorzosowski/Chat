66692fbed9164029f273965b3c5540855c4c37d2
Grzegorz <osowskigrzegorz95@gmail.com>
2023-05-29 13:53:44 +0200

    variables moved from another files

--- chatServer/config.ts
+++ chatServer/config.ts
@@ -1,2 +1,4 @@
 export const port: any = process.env.PORT || 3001;
 export const database: string = process.env.DATABASE || 'mongodb://root:root_password@localhost:27017/';
+export const secret = process.env.SECRET || 'default-secret-key';
+export const cookieMaxAge = 1000 * 60 * 60 * 24; // 24 hours
