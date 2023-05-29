d8140a1d035a68c8c72909505fbbb0b30e7aec03
Grzegorz <osowskigrzegorz95@gmail.com>
2023-05-29 13:55:42 +0200

    DB access functions - Created file

---
+++ chatServer/lib/dbRequestFunctions.ts
@@ -0,0 +1,37 @@
+import { Types } from 'mongoose';
+import User, { UserType, UserWithId } from '../db/models/User';
+import Chat from '../db/models/Chat';
+import Messages from '../db/models/Messages';
+
+export function findUserByEmail(email: string) {
+    return User.findOne<UserWithId>({ email: email });
+}
+
+export function findUserById(id: string) {
+    return User.findById({ _id: id });
+}
+
+export function updateUser(id: string | Types.ObjectId, update: Partial<UserType>) {
+    return User.updateOne({ _id: id }, { $set: update });
+}
+
+export function findChatById(id: string) {
+    return Chat.findById({ _id: id });
+}
+
+export function findChatByName(chatName: string) {
+    return Chat.find({ chatName: chatName });
+}
+
+export function findChatByMembers(members: string[]) {
+    return Chat.findOne({
+        members: {
+            $size: 2,
+            $all: members,
+        },
+    });
+}
+
+export function findMessagesByChatId(chatId: string) {
+    return Messages.find({ chatID: chatId });
+}
