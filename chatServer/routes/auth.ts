ca25835478a144eb43ab92b9f56c7f37c1b48e76
Grzegorz <osowskigrzegorz95@gmail.com>
2023-05-29 13:54:27 +0200

    deleted

--- chatServer/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const routerAuth = express.Router();
-
-routerAuth.get('/login', function (req, res, next) {console.log("Redirect to /login")});
- 
-export = routerAuth;
