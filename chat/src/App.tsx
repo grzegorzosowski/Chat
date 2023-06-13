ee9d22608588f4ac6974355dbf64b25ba5aab2d1
Grzegorz <osowskigrzegorz95@gmail.com>
2023-06-13 10:31:27 +0200

    Extracted routes from Layout component

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -1,24 +1,27 @@
 import { SnackbarProvider } from 'notistack';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
 import { apiSlice } from './features/api/apiSlice';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import Layout from './components/Layout';
 import { CurrentThemeProvider } from './features/currentThemeProvider';
 import { UserProvider } from './UserProvider';
+import { Routes } from './Routes';
 
 export default function App() {
   return (
     <ApiProvider api={apiSlice}>
-      <Provider store={store}>
+      <ReduxProvider store={store}>
         <CurrentThemeProvider>
           <UserProvider>
             <SnackbarProvider maxSnack={3} autoHideDuration={3000} style={{ fontFamily: 'Roboto, sans-serif' }} >
-              <Layout />
+              <Layout>
+                <Routes />
+              </Layout>
             </SnackbarProvider>
           </UserProvider>
         </CurrentThemeProvider>
-      </Provider>
+      </ReduxProvider>
     </ApiProvider>
   );
 }
