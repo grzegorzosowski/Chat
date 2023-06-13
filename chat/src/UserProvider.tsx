51cf1e14c18643e58e3822a1c23a20229b930f1f
Grzegorz <osowskigrzegorz95@gmail.com>
2023-06-13 10:32:05 +0200

    Improvements

--- chat/src/UserProvider.tsx
+++ chat/src/UserProvider.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useContext, useEffect, useState, createContext } from 'react';
-import LoadingPage from './components/LoadingPage';
 import { useGetAuthUserQuery } from './features/api/apiSlice'
-
-export type User = {
-    _id: string;
-    nick: string;
-    email: string;
-    verified: boolean;
-}
-
-const Context = createContext<User | undefined>(undefined);
+import LoadingCircle from './components/LoadingCircle';
 
 export function useUser() {
-    return useContext(Context);
+    const { data } = useGetAuthUserQuery();
+    return data;
 }
 
 type UserProviderType = {
@@ -21,30 +12,11 @@ type UserProviderType = {
 }
 
 export function UserProvider({ children }: UserProviderType) {
-    const [isLoading, setIsLoading] = useState(true);
-    const [user, setUser] = useState<User | undefined>();
-
-    //const authUser = useGetAuthUserQuery({}).data as User
-    //useEffect to check by Chris
-    useEffect(() => {
-        // setIsLoading(true);
-        // setUser(authUser)
-        // setIsLoading(false)
-
-        fetch('api/user', { method: 'GET' })
-            .then((res) => res.json() as Promise<User>)
-            .then((user) => {
-                setUser(user);
-            })
-            .catch(() => {
-                setUser(undefined)
-            })
-            .finally(() => setIsLoading(false));
-    }, []);
+    const { isLoading } = useGetAuthUserQuery();
 
     if (isLoading) {
-        return <LoadingPage />;
+        return <LoadingCircle />;
     }
 
-    return <Context.Provider value={user}>{children}</Context.Provider>;
+    return <>{children}</>;
 }
